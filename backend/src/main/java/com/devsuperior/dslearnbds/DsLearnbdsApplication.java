263ba5ed7f37ef841e575ca3346902fc36d88e4b
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2020-12-10 15:41:21 -0300

    Backend project created

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/DsLearnbdsApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslearnbds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsLearnbdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsLearnbdsApplication.class, args);
+	}
+
+}
