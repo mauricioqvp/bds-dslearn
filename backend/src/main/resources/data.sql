806c55305d2e1b1b27a400f47fd4c4a0d1f1e271
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2020-12-26 21:25:48 -0300

    Enrollment seed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -26,3 +26,5 @@ INSERT INTO tb_section  (title, description, position, img_Uri, resource_id, pre
 INSERT INTO tb_section  (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2','Neste capitulo vamos continuar',2,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1vGLoOegt5KuNntjFXvFhTDTxxY1I2ycnxw&usqp=CAU',1,1);
 INSERT INTO tb_section  (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 3','Neste capitulo vamos finalizar',3,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1vGLoOegt5KuNntjFXvFhTDTxxY1I2ycnxw&usqp=CAU',1,2);
 
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1,1, TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00Z', null, true, false);
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2,1, TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00Z', null, true, false);
